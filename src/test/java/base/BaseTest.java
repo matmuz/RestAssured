7ab3c1ae5452874648d5a65f18f032a34ab49a25
Mateusz Muzyka <mmuzyka@sii.pl>
2021-04-27 12:14:08 +0200

    avoiding test dependency part 1

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,11 +3,17 @@ package base;
 import data.JsonPicker;
 import data.UserProvider;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeAll;
 
 import java.io.IOException;
 
-import static io.restassured.RestAssured.basic;
+import static io.restassured.RestAssured.*;
+
+//TODO: Utility methods should be placed elsewhere
 
 public class BaseTest {
 
@@ -23,5 +29,68 @@ public class BaseTest {
         RestAssured.baseURI = "https://restful-booker.herokuapp.com";
         RestAssured.basePath = "/booking";
         RestAssured.authentication = basic(admin.getUsername(), admin.getPassword());
+
+        healthCheck();
+        createToken();
+    }
+
+    public static void healthCheck(){
+        Response response = given().when()
+                .get(baseURI + "/ping")
+                .then()
+                .extract()
+                .response();
+
+        int statusCode = response.statusCode();
+
+        if (statusCode != 201) {
+            System.out.println("Health check failed with status " + statusCode);
+            System.exit(1);
+        }
+    }
+
+    public static void createToken(){
+
+        JSONObject body = new JSONObject();
+        body.put("username", admin.getUsername());
+        body.put("password", admin.getPassword());
+
+        Response response = given().contentType(ContentType.JSON)
+                .body(body.toString())
+                .when()
+                .post(baseURI + "/auth")
+                .then()
+                .extract()
+                .response();
+
+        JsonPath json = response.jsonPath();
+
+        token = json.getString("token");
+    }
+
+    public static JSONObject prepareNewBooking(){
+
+        JSONObject bookingDates = new JSONObject();
+        bookingDates.put("checkin", "2021-01-23");
+        bookingDates.put("checkout", "2021-02-01");
+
+        JSONObject booking = new JSONObject();
+        booking.put("firstname", user.getFirstName());
+        booking.put("lastname", user.getLastName());
+        booking.put("totalprice", "115");
+        booking.put("depositpaid", "15");
+        booking.put("bookingdates", bookingDates);
+        booking.put("additionalneeds", "monster");
+
+        return booking;
+    }
+
+    public static JSONObject partiallyUpdateBooking(){
+
+        JSONObject update = new JSONObject();
+        update.put("firstname", user.getFirstName());
+        update.put("lastname", user.getLastName());
+
+        return update;
     }
 }
--- src/test/java/tests/BookingTest.java
+++ src/test/java/tests/BookingTest.java
@@ -1,72 +1,19 @@
 package tests;
 
 import base.BaseTest;
-import data.UserProvider;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.assertj.core.api.Assertions;
-import org.json.JSONObject;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 
 import static io.restassured.RestAssured.*;
 
-/**
- * given - specification
- * when - request
- * then - assertion
- */
+//TODO: Avoid test dependency - all tests should be able to run separately - can be done by extending tests to be done from scratch
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class BookingTest extends BaseTest {
 
-    @Test()
-    @Order(1)
-    public void shouldGetResponse() {
-
-        Response response = given().when()
-                .get(baseURI + "/ping")
-                .then()
-                .extract()
-                .response();
-
-        int statusCode = response.statusCode();
-
-        if (statusCode != 201) {
-            System.out.println("Health check failed with status " + statusCode);
-            System.exit(1);
-        }
-    }
-
     @Test
-    @Order(2)
-    public void shouldCreateToken() {
-
-        JSONObject body = new JSONObject();
-        body.put("username", admin.getUsername());
-        body.put("password", admin.getPassword());
-
-        Response response = given().contentType(ContentType.JSON)
-                .body(body.toString())
-                .when()
-                .post(baseURI + "/auth")
-                .then()
-                .extract()
-                .response();
-
-        JsonPath json = response.jsonPath();
-
-        token = json.getString("token");
-
-        Assertions.assertThat(json.getString("token"))
-                .isNotEmpty();
-    }
-
-    @Test
-    @Order(3)
     public void shouldReturnListOfBookings() {
 
         Response response = given()
@@ -87,24 +34,11 @@ public class BookingTest extends BaseTest {
     }
 
     @Test
-    @Order(4)
     public void shouldCreateNewBooking() {
-
-        JSONObject bookingDates = new JSONObject();
-        bookingDates.put("checkin", "2021-01-23");
-        bookingDates.put("checkout", "2021-02-01");
-
-        JSONObject booking = new JSONObject();
-        booking.put("firstname", user.getFirstName());
-        booking.put("lastname", user.getLastName());
-        booking.put("totalprice", "115");
-        booking.put("depositpaid", "15");
-        booking.put("bookingdates", bookingDates);
-        booking.put("additionalneeds", "monster");
 
         Response response = given()
                 .contentType(ContentType.JSON)
-                .body(booking.toString())
+                .body(prepareNewBooking().toString())
                 .when()
                 .post(baseURI + basePath)
                 .then()
@@ -123,7 +57,6 @@ public class BookingTest extends BaseTest {
     }
 
     @Test
-    @Order(5)
     public void shouldGetCreatedBooking() {
 
         Response response = given()
@@ -143,7 +76,6 @@ public class BookingTest extends BaseTest {
     }
 
     @Test
-    @Order(6)
     public void shouldDeleteCreatedBooking() {
 
         Response response = given().header("Cookie", "token=" + token)
@@ -158,7 +90,6 @@ public class BookingTest extends BaseTest {
     }
 
     @Test
-    @Order(7)
     public void shouldNotFindDeletedBooking() {
 
         Response response = given().when()
@@ -172,26 +103,11 @@ public class BookingTest extends BaseTest {
     }
 
     @Test
-    @Order(8)
     public void shouldCreateAnotherBooking() {
-
-        UserProvider anotherUser = new UserProvider();
-
-        JSONObject bookingDates = new JSONObject();
-        bookingDates.put("checkin", "2021-01-23");
-        bookingDates.put("checkout", "2021-02-01");
-
-        JSONObject booking = new JSONObject();
-        booking.put("firstname", anotherUser.getFirstName());
-        booking.put("lastname", anotherUser.getLastName());
-        booking.put("totalprice", "225");
-        booking.put("depositpaid", "25");
-        booking.put("bookingdates", bookingDates);
-        booking.put("additionalneeds", "cookie");
 
         Response response = given()
                 .contentType(ContentType.JSON)
-                .body(booking.toString())
+                .body(prepareNewBooking().toString())
                 .when()
                 .post(baseURI + basePath)
                 .then()
@@ -206,20 +122,15 @@ public class BookingTest extends BaseTest {
         id = json.getString("bookingid");
 
         Assertions.assertThat(json.getString("booking.firstname"))
-                .isEqualTo(anotherUser.getFirstName());
+                .isEqualTo(user.getFirstName());
     }
 
     @Test
-    @Order(9)
     public void shouldPartiallyUpdateBooking() {
-
-        JSONObject update = new JSONObject();
-        update.put("firstname", user.getFirstName());
-        update.put("lastname", user.getLastName());
 
         Response response = given().header("Cookie", "token=" + token)
                 .contentType(ContentType.JSON)
-                .body(update.toString())
+                .body(partiallyUpdateBooking().toString())
                 .when()
                 .patch(baseURI + basePath + "/" + id)
                 .then()
@@ -231,7 +142,6 @@ public class BookingTest extends BaseTest {
     }
 
     @Test
-    @Order(10)
     public void shouldGetUpdatedBooking() {
 
         Response response = given().when()
